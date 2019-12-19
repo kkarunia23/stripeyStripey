49258869e7f13f40c88f701f401633d3c6cffcf5
kkarunia23 <kenneth.karunia@gmail.com>
2019-12-19 19:24:08 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# stripeyStripey
-Created with CodeSandbox
+# Using Checkout for one-time purchases
+
+Building payment form UI from scratch is difficult -- input field validation, error message handing, and localization are just a few things to think about when designing a simple checkout flow.
+
+We built [Checkout](https://stripe.com/docs/payments/checkout) to do that work for you so now you can focus on building the best storefront experience for your customers.
+
+Once your customer is ready to pay, use Stripe.js to redirect them to the URL of your Stripe hosted payment page. It's so simple you don't even need a server! 🥳
+
+<img src="./checkout-demo.gif" alt="A gif of the Checkout payment page rendering" align="center">
+
+**Features:**
+
+- Localization in 14 different languages 🌍
+- Built-in Apple Pay support 🍎
+- Built-in dynamic 3D Secure (ready for SCA) 🔔
+- Plans to support more payment methods 🔮
+
+For more features see the [Checkout documentation](https://stripe.com/docs/payments/checkout).
+
+There are two integrations: [client-only](./client-only) and [client-and-server](./client-and-server). This CodeSandbox only shows the client-and-server integration, to see the client-only code go to the GitHub repository:
+
+https://github.com/stripe-samples/checkout-one-time-payments
+
+<!-- prettier-ignore -->
+|     | client-only | client-and-server
+:--- | :---: | :---:
+🔨 **Prebuilt checkout page.** Create a payment page that is customizable with your business' name and logo. | ✅  | ✅ |
+🔢 **Dynamic checkout amounts.** Dynamically define product amounts rather than relying on predefined SKUs.  | ❌  | ✅ |
+⌛ **Capture payments later.** Optionally split the capture and authorization steps to place a hold on the card and charge later. | ❌ | ✅ |
+
+## How to run locally
+
+If you want to run the recipe locally, copy the .env.example file to your own .env file in this directory:
+
+```
+cp .env.example .env
+```
+
+You will need a Stripe account with its own set of [API keys](https://stripe.com/docs/development#api-keys).
+
+## FAQ
+
+Q: Why did you pick these frameworks?
+
+A: We chose the most minimal framework to convey the key Stripe calls and concepts you need to understand. These demos are meant as an educational tool that helps you roadmap how to integrate Stripe within your own system independent of the framework.
+
+Q: Can you show me how to build X?
+
+A: We are always looking for new recipe ideas, please email dev-samples@stripe.com with your suggestion!
+
+## Author(s)
+
+[@adreyfus-stripe](https://twitter.com/adrind)
---
+++ client-and-server/client/script.js
@@ -0,0 +1,110 @@
+// The max and min number of photos a customer can purchase
+var MIN_PHOTOS = 1;
+var MAX_PHOTOS = 10;
+
+var basicPhotoButton = document.getElementById("basic-photo-button");
+document
+  .getElementById("quantity-input")
+  .addEventListener("change", function(evt) {
+    // Ensure customers only buy between 1 and 10 photos
+    if (evt.target.value < MIN_PHOTOS) {
+      evt.target.value = MIN_PHOTOS;
+    }
+    if (evt.target.value > MAX_PHOTOS) {
+      evt.target.value = MAX_PHOTOS;
+    }
+  });
+
+/* Method for changing the product quantity when a customer clicks the increment / decrement buttons */
+var updateQuantity = function(evt) {
+  if (evt && evt.type === "keypress" && evt.keyCode !== 13) {
+    return;
+  }
+
+  var isAdding = evt.target.id === "add";
+  var inputEl = document.getElementById("quantity-input");
+  var currentQuantity = parseInt(inputEl.value);
+
+  document.getElementById("add").disabled = false;
+  document.getElementById("subtract").disabled = false;
+
+  // Calculate new quantity
+  var quantity = isAdding ? currentQuantity + 1 : currentQuantity - 1;
+
+  inputEl.value = quantity;
+  document.getElementById("total").textContent = quantity * 5;
+
+  // Disable the button if the customers hits the max or min
+  if (quantity === MIN_PHOTOS) {
+    document.getElementById("subtract").disabled = true;
+  }
+  if (quantity === MAX_PHOTOS) {
+    document.getElementById("add").disabled = true;
+  }
+};
+
+/* Attach method */
+Array.from(document.getElementsByClassName("increment-btn")).forEach(
+  element => {
+    element.addEventListener("click", updateQuantity);
+  }
+);
+
+/* Handle any errors returns from Checkout  */
+var handleResult = function(result) {
+  if (result.error) {
+    var displayError = document.getElementById("error-message");
+    displayError.textContent = result.error.message;
+  }
+};
+
+// Create a Checkout Session with the selected quantity
+var createCheckoutSession = function(stripe) {
+  var inputEl = document.getElementById("quantity-input");
+  var quantity = parseInt(inputEl.value);
+
+  return fetch("/create-checkout-session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      quantity: quantity
+    })
+  }).then(function(result) {
+    return result.json();
+  });
+};
+
+// Handle any errors returned from Checkout
+var handleResult = function(result) {
+  if (result.error) {
+    var displayError = document.getElementById("error-message");
+    displayError.textContent = result.error.message;
+  }
+};
+
+/* Get your Stripe public key to initialize Stripe.js */
+fetch("/public-key")
+  .then(function(result) {
+    return result.json();
+  })
+  .then(function(json) {
+    var publicKey = json.publicKey;
+    var stripe = Stripe(publicKey);
+    // Setup event handler to create a Checkout Session on submit
+    document.querySelector("#submit").addEventListener("click", function(evt) {
+      createCheckoutSession().then(function(data) {
+        stripe
+          .redirectToCheckout({
+            sessionId: data.sessionId
+          })
+          .then(handleResult);
+      });
+    });
+  });
+
+// Hide banner if window is too small or in iframe
+if (window.outerHeight < 600 || window.location !== window.parent.location) {
+  document.querySelector(".banner").classList.add("hidden");
+}
